839ea7b0760500a8c8e5204d8a1dd985773ae6f2
natarazworld <natarazworld@gmail.com>
2022-07-21 17:20:18 +0530

    Add files via upload
    jdbc first App  imporved

--- ConnTest.java
+++ ConnTest.java
@@ -5,12 +5,7 @@ public class  ConnTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		//step1:: Activate oracle thin jdbc driver s/w by registering jdbc driver
-		 // class object with DriverMAnager serivce
-		          //a)  create jdbc driver class obj
-				  oracle.jdbc.driver.OracleDriver driver=new oracle.jdbc.driver.OracleDriver();
-				  //b)  register with DriverManager service
-				   DriverManager.registerDriver(driver);
+				Class.forName("oracle.jdbc.driver.OracleDriver");
 
         //step2:   Estalish  the connection with oracle Db s/w
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system" ,"tiger");
